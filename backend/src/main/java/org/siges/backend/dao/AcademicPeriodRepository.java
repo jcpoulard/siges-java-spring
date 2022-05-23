d75abd21de7b68adf2c1f8568e18771bc435835e
jcpoulard <jcpoulard@gmail.com>
2022-05-23 12:06:44 -0400

    Update AcademicPeriodRepository.java

--- backend/src/main/java/org/siges/backend/dao/AcademicPeriodRepository.java
+++ backend/src/main/java/org/siges/backend/dao/AcademicPeriodRepository.java
@@ -2,7 +2,9 @@ package org.siges.backend.dao;
 
 import org.siges.backend.entity.AcademicPeriod;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface AcademicPeriodRepository extends JpaRepository<AcademicPeriod, Integer> {
 
 }
